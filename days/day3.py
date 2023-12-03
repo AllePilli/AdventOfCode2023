0da1495b5429eb1339c8a0c682266e5ac2a26949
Allesio <allesio.pellicciotta@gmail.com>
2023-12-03 12:00:35 +0100

    Use regex in part1 instead of _Token

--- days/day3.py
+++ days/day3.py
@@ -1,69 +1,43 @@
-from collections import namedtuple
+import re
 
 part1_exp_test_result = 4361
 part1_exp_result = 536202
 
-_Point = namedtuple('_Point', ['x', 'y'])
+_part1_num_re = r"(\d+)"
 
 
-class _Token:
-    def __init__(self, position: _Point, value: str):
-        self.position = position
-        self.value = value
+def _get_edges(y: int, xspan: tuple[int, int], grid_dimensions: tuple[int, int]) -> list[tuple[int, int]]:
+    start, end = xspan
+    height, width = grid_dimensions
+    edged = []
 
-    def __repr__(self):
-        return f"Token({self.position}, {self.value})"
+    if y > 1:
+        edged.extend(
+            [(ix, y - 1) for ix in range(start - 1, end + 1) if ix in range(width)]
+        )
 
+    if start > 0:
+        edged.append((start - 1, y))
+    if end < width:
+        edged.append((end, y))
 
-def tokenize(lines: list[str]) -> list[_Token]:
-    tokens: list[_Token] = []
+    if y + 1 < height:
+        edged.extend(
+            [(ix, y + 1) for ix in range(start - 1, end + 1) if ix in range(width)]
+        )
 
-    def get_int_str(p: _Point) -> str:
-        line = lines[p.y]
-        x = p.x
-        num = ''
-
-        while x < len(line) and line[x].isdigit():
-            num += line[x]
-            x += 1
-
-        return num
-
-    for y, line in enumerate(lines):
-        x = 0
-        while x < len(line):
-            if line[x].isdigit():
-                position = _Point(x, y)
-                tokens.append(_Token(position, get_int_str(position)))
-                x += len(tokens[-1].value)
-            else:
-                x += 1
-    return tokens
+    return edged
 
 
 def part1(lines: list[str]) -> int:
     _sum = 0
+    grid_dimensions = len(lines), len(lines[0])
 
-    for token in tokenize(lines):
-        x, y = token.position
-        edges = []
+    for y, line in enumerate(lines):
+        for match in re.finditer(_part1_num_re, line):
+            edges = _get_edges(y, match.span(), grid_dimensions)
 
-        if y > 1:
-            edges.extend(
-                [_Point(ix, y - 1) for ix in range(x - 1, x + len(token.value) + 1) if ix in range(len(lines[y - 1]))]
-            )
-
-        if x > 0:
-            edges.append(_Point(x - 1, y))
-        if x + len(token.value) < len(lines[y]):
-            edges.append(_Point(x + len(token.value), y))
-
-        if y + 1 < len(lines):
-            edges.extend(
-                [_Point(ix, y + 1) for ix in range(x - 1, x + len(token.value) + 1) if ix in range(len(lines[y + 1]))]
-            )
-
-        if any(lines[iy][ix] != '.' and not lines[iy][ix].isdigit() for ix, iy in edges):
-            _sum += int(token.value)
+            if any(lines[iy][ix] != '.' and not lines[iy][ix].isdigit() for ix, iy in edges):
+                _sum += int(match.group())
 
     return _sum
