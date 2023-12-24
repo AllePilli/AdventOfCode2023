c7f87a76d02ac23cc1706ff83e26208c8d7fbbcf
Allesio <allesio.pellicciotta@gmail.com>
2023-12-24 13:20:50 +0100

    Solved Day 7 part 2

--- days/day7.py
+++ days/day7.py
@@ -2,38 +2,44 @@ from collections import Counter
 
 part1_exp_test_result = 6440
 part1_exp_result = 251106089
-
-_card_vals = {
-    'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2, '1': 1
-}
+part2_exp_test_result = 5905
 
 
-def _line_key(line: str) -> tuple[int, ...]:
-    hand = line.split(' ')[0]
+def _get_type(hand: str) -> int:
     freq = Counter(hand)
-    _type = 0
+    return ([1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 2, 2], [1, 1, 3], [2, 3], [1, 4], [5]).index(sorted(freq.values()))
 
-    match sorted(freq.values(), reverse=True):
-        case [5]:
-            _type = 7
-        case [4, 1]:
-            _type = 6
-        case [3, 2]:
-            _type = 5
-        case [3, 1, 1]:
-            _type = 4
-        case [2, 2, 1]:
-            _type = 3
-        case [2, 1, 1, 1]:
-            _type = 2
-        case [1, 1, 1, 1, 1]:
-            _type = 1
 
-    return tuple(_card_vals[hand[i - 1]] if i > 0 else _type for i in range(len(hand) + 1))
+def _hand_score(hand: str) -> tuple[int, ...]:
+    _type = _get_type(hand)
+    hand_score = ['23456789TJQKA'.index(x) for x in hand]
+    return _type, *hand_score
+
+
+def hand(h, part1):
+    if part1:
+        h = h.replace('J', 'X')
+    h2 = ['J23456789TXQKA'.index(i) for i in h]
+    ts = []
+    for r in '23456789TQKA':
+        c = Counter(h.replace('J', r))
+        p = tuple(sorted(c.values()))
+        t = [(1, 1, 1, 1, 1), (1, 1, 1, 2), (1, 2, 2), (1, 1, 3), (2, 3), (1, 4), (5,)].index(p)
+        ts.append(t)
+    return (max(ts), *h2)
 
 
 def part1(lines: list[str]) -> int:
     _sum = 0
-    for rank, line in enumerate(sorted(lines, key=_line_key), start=1):
-        _sum += rank * int(line.split(' ')[1])
+    hands = [x.split() for x in lines]
+    for rank, line in enumerate(sorted(hands, key=lambda x: _hand_score(x[0])), start=1):
+        _sum += rank * int(line[1])
     return _sum
+
+
+def part2(lines: list[str]) -> int:
+    h = sorted((hand(h, False), int(b)) for h, b in (l.split() for l in lines))
+    t = 0
+    for i, (_, b) in enumerate(h):
+        t += i * b + b
+    return t
